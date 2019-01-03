21180a7d5e2d191c29612def6b7d46078d72079a
Sjbrimley26 <sjbrimley26@live.com>
2019-01-03 10:03:20 -0700

    Added links to projects
    Added a couple more projects

--- src/assets/components/ProjectBlock.vue
+++ src/assets/components/ProjectBlock.vue
@@ -4,7 +4,8 @@
       <h2>Projects</h2>
       <div class="projectBlock">
         <div class="projectImage" 
-          v-bind:style="{ backgroundImage: `url('${projects[currentProject].image}')` }" 
+          v-bind:style="{ backgroundImage: `url('${projects[currentProject].image}')` }"
+          v-on:click="navTo(projects[currentProject].link)"
         >
         </div>
         <div class="description">
@@ -24,7 +25,13 @@
 </template>
 
 <script>
-import { pinterestPic, fairRentalsPic } from "../images";
+import { 
+  pinterestPic,
+  fairRentalsPic,
+  uploaderPic,
+  rougelikePic
+} from "../images";
+
 export default {
   name: "ProjectBlock",
   data() {
@@ -36,19 +43,43 @@ export default {
             Although the project is definitely still in the MVP phase, I'm proud of what we 
             accomplished with limited skills and time, and I definitely learned a ton from the 
             experience.`,
-          image: pinterestPic
+          image: pinterestPic,
+          link: "https://github.com/chingu-voyage4/Bears-Team-26" 
         },
         {
-          description: `During another Chingu project, our team created a somewhat original
+          description: `During another Chingu project, our team created an original
             apartment review site. It features authentication, comments, and Google Maps
             integration. This was my first Chingu project and fortunately one of my team
             members was incredibly helpful in showing me what to do and I was able to start 
             contributing significantly once the project really got going.`,
-          image: fairRentalsPic
+          image: fairRentalsPic,
+          link: "https://github.com/chingu-voyage3/fair-rentals"
+        },
+        {
+          description: `While learning Docker, I decided to make a server for uploading and downloading
+            all sorts of files. Using Docker compose, I set up a main api server which pipes requests
+            to replicated download / upload servers, allowing fast, non-blocking connections. Will eventually incorporate
+            Nginx to serve the static files.`,
+          image: uploaderPic,
+          link: "https://github.com/Sjbrimley26/uploader-docker"
+        },
+        {
+          image: rougelikePic,
+          link: "https://github.com/Sjbrimley26/redRouge",
+          description: `As a side project, I'm working on a rougelike game. Although
+            it is still pretty far from playable, working on it gave me the opportunity to study / implement
+            a number of previously unknown algorithms / data structures. One aspect I'm really proud of was originally the levels took anywhere from 8 - 40 seconds to load and I eventually
+            got it to under one second on my desktop.`
         }
       ],
 
       currentProject: 0
+    }
+  },
+
+  methods: {
+    navTo(url) {
+      window.open(url);
     }
   }
 }
@@ -126,11 +157,13 @@ export default {
 
   .description {
     padding: 0 20px;
-    font-family: Neuton;
-    font-size: 0.85em;
+    font-family: "Neuton", Arial;
+    font-size: calc(0.6em + 100vw / 110);
+    display: flex;
+    align-items: center;
 
-    @media(min-width: 460px) {
-      font-size: 1em;
+    @media(min-width: 1000px) {
+      font-size: 1.2em;
     }
   }
 
@@ -140,6 +173,7 @@ export default {
     box-shadow: 0 0 10px gray;
     background-size: cover;
     background-position: center;
+    cursor: pointer;
   }
 </style>
 
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,11 +4,15 @@ import profilePic from "./profile1.jpg";
 
 import pinterestPic from "./projects/pinterest_clone.jpg";
 import fairRentalsPic from "./projects/fair_rentals.jpg";
+import uploaderPic from "./projects/docker_uploader.jpg";
+import rougelikePic from "./projects/rougelike.jpg";
 
 export {
   elvisPic,
   couplePic,
   profilePic,
   pinterestPic,
-  fairRentalsPic
+  fairRentalsPic,
+  uploaderPic,
+  rougelikePic
 };
