eab2818f182464564f739198575d6994852b5399
Sjbrimley26 <sjbrimley26@live.com>
2019-01-16 16:00:59 -0700

    Removed service worker - not ready yet

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import "./assets/styles/global.scss";
 
 import Vue from "vue";
 import App from "./App.vue";
-import * as serviceWorker from "./registerServiceWorker";
-
-serviceWorker.register();
 
 const app = new Vue({
   el: "#app",
